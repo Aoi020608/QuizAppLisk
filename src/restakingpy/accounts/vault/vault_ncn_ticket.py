8d523fbe11eda999fbb6dd52f365d7780cd4a11d
aoikurokawa <ht.so.ik0102@gmail.com>
2024-11-17 08:46:19 +0900

    feat: vault_ncn_ticket

--- src/restakingpy/accounts/vault/vault_ncn_ticket.py
+++ src/restakingpy/accounts/vault/vault_ncn_ticket.py
@@ -4,38 +4,32 @@ from solders.pubkey import Pubkey
 
 from restakingpy.accounts.core.slot_toggle import SlotToggle
 
-class VaultNcnSlasherTicket:
+class VaultNcnTicket:
 
-    discriminator: typing.ClassVar = 5
+    discriminator: typing.ClassVar = 3
 
     vault: Pubkey
     ncn: Pubkey
-    slasher: Pubkey
 
-    max_slashable_per_epoch: int
     index: int
     state: SlotToggle
     bump: int
 
-    # Initialize a VaultNcnSlasherTicket instance with required attributes
-    def __init__(self, vault: Pubkey, ncn: Pubkey, slasher: Pubkey, max_slashable_per_epoch: int, index: int, state: SlotToggle, bump: int):
+    # Initialize a VaultNcnTicket instance with required attributes
+    def __init__(self, vault: Pubkey, ncn: Pubkey, index: int, state: SlotToggle, bump: int):
         self.vault = vault
         self.ncn = ncn
-        self.slasher = slasher
 
-        self.max_slashable_per_epoch = max_slashable_per_epoch
-        self.state = state
         self.index = index
+        self.state = state
         self.bump = bump
 
-    # Display VaultNcnSlasherTicket
+    # Display VaultNcnTicket
     def __str__(self):
         return (
-            f"VaultNcnSlasherTicket(\n"
+            f"VaultNcnTicket(\n"
             f"  vault={self.vault},\n"
             f"  ncn={self.ncn},\n"
-            f"  slasher={self.slasher},\n"
-            f"  max_slashable_per_epoch={self.max_slashable_per_epoch},\n"
             f"  index={self.index},\n"
             f"  state={self.state},\n"
             f"  bump={self.bump},\n"
@@ -43,8 +37,8 @@ class VaultNcnSlasherTicket:
         )
 
     @staticmethod
-    def deserialize(data: bytes) -> "VaultNcnSlasherTicket":
-        """Deserializes bytes into a VaultNcnSlasherTicket instance."""
+    def deserialize(data: bytes) -> "VaultNcnTicket":
+        """Deserializes bytes into a VaultNcnTicket instance."""
         
         # Define offsets for each field
         offset = 0
@@ -56,12 +50,6 @@ class VaultNcnSlasherTicket:
         ncn = Pubkey.from_bytes(data[offset:offset + 32])
         offset += 32
 
-        slasher = Pubkey.from_bytes(data[offset:offset + 32])
-        offset += 32
-
-        max_slashable_per_epoch = int.from_bytes(data[offset:offset + 8], byteorder='little')
-        offset += 8
-
         index = int.from_bytes(data[offset:offset + 8], byteorder='little')
         offset += 8
 
@@ -70,26 +58,24 @@ class VaultNcnSlasherTicket:
 
         bump = int.from_bytes(data[offset:offset + 1])
 
-        # Return a new VaultNcnSlasherTicket instance with the deserialized data
-        return VaultNcnSlasherTicket(
+        # Return a new VaultNcnTicket instance with the deserialized data
+        return VaultNcnTicket(
             vault,
             ncn,
-            slasher,
-            max_slashable_per_epoch,
             index,
             state,
             bump,
         )
 
     @staticmethod
-    def seeds(vault: Pubkey, ncn: Pubkey, slasher: Pubkey) -> typing.List[bytes]:
+    def seeds(vault: Pubkey, ncn: Pubkey) -> typing.List[bytes]:
         """Return the seeds used for generating PDA."""
-        return [b"vault_slasher_ticket", bytes(vault), bytes(ncn), bytes(slasher)]
+        return [b"vault_ncn_ticket", bytes(vault), bytes(ncn)]
     
     @staticmethod
-    def find_program_address(program_id: Pubkey, vault: Pubkey, ncn: Pubkey, slasher: Pubkey) -> typing.Tuple[Pubkey, int, typing.List[bytes]]:
+    def find_program_address(program_id: Pubkey, vault: Pubkey, ncn: Pubkey) -> typing.Tuple[Pubkey, int, typing.List[bytes]]:
         """Finds the program-derived address (PDA) for the given seeds and program ID."""
-        seeds = VaultNcnSlasherTicket.seeds(vault, ncn, slasher)
+        seeds = VaultNcnTicket.seeds(vault, ncn)
         
         # Compute PDA and bump using seeds (requires solders Pubkey functionality)
         pda, bump = Pubkey.find_program_address(seeds, program_id)
